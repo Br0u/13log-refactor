1a562b921326443e649134beff7d029ab595df78
Brou <NightKnightBrou@foxmail.com>
2026-06-11 18:47:25 +0800

    Polish visual theme styling

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,10 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
         <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Iosevka+Charon+Mono:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         />
